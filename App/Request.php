1761929f3a12e76b5e5fb230a101657cca0e74a6
Pavel Tashev <p.d.tashev@gmail.com>
2016-10-05 16:26:01 +0100

    Fix for the native PHP function getallheaders
    Here we provide an alternative for the native PHP function getallheaders, because there are cases when it does not work.

--- App/Request.php
+++ App/Request.php
@@ -51,7 +51,7 @@ class Request
      */
     public static function getHeader($key)
     {
-        return getallheaders()[$key];
+        return self::getallheaders()[$key];
     }
 
     /**
@@ -117,5 +117,23 @@ class Request
         }
 
         return self::$inputData;
+    }
+    
+    /**
+     * @return array
+     * @desc Get all headers
+     * @return array
+     */
+    private static function getallheaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $name => $value)
+        {
+            if (substr($name, 0, 5) == 'HTTP_')
+            {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
     }
 }
